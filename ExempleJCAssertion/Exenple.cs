f8f9fd155f1823d8027f33f601226272e84e7692
Jean-Claude Parent <noursicain@gmail.com>
2019-03-17 11:22:47 -0400

    Commit initial

---
+++ ExempleJCAssertion/Exenple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JCAssertionCore;
+
+namespace ExempleJCAssertion
+{
+    [TestClass]
+    public class Exenple
+    {
+        /// <summary>
+        /// exemple d'utilisation du client oracle
+        /// pour un test
+        /// </summary>
+        [TestMethod]
+        public void Exemple()
+        {
+            JCASQLClient SQLClient = new JCASQLClient();
+            // Utiliser le user et password oracle de la bd de débveloppement jcassertion
+            // mettez votre connection pour la remplacer
+            // Comme ion ne spécifie pas de serveur
+            // ce sera localhost
+            SQLClient.InitConnection("JCA","JCA");
+            Assert.IsTrue(SQLClient.SQLAssert(
+                "select count(*) from JCATest where IDTEST ='existepas'", 0));
+            
+        }
+    }
+}
